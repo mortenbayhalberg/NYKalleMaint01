45bba754718c3a8913008655e30f66007df9a02b
mortenbayhalberg <mortenbayhalberg@outlook.com>
2016-11-06 17:50:08 +0100

    Portering til Britta-pc
    Portering af vedligeholdelse til Britta-pc

--- NYKalleMaintV01/Models/NyKalleDB.Designer.cs
+++ NYKalleMaintV01/Models/NyKalleDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mh.MP\Documents\Visual Studio 2015\Projects\NYKalleMaintV01\NYKalleMaintV01\Models\NyKalleDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\mh\Documents\GitHub\NYKalleMaint01\NYKalleMaintV01\Models\NyKalleDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
